d5d5b9ea72a9e859fa934df355b56d41457c7819
Pito Salas <pitosalas@users.noreply.github.com>
2023-02-04 13:50:18 -0500

    checkpoint

--- c/fork1.c
+++ c/fork1.c
@@ -2,20 +2,33 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
+
 
 int main()
 {
+    char   buf[100];
+    sprintf(buf, "Starting... \n");
+    write(1, buf, strlen(buf));
+
     int pid = fork();
     if (pid == 0)
     {
         // Child Only
-        printf("             >>> (pid: %o) I am child process\n", getpid());
+        sprintf(buf, ">>> (pid: %o) I am child process\n", getpid());
+        write(1, buf, strlen(buf));
         exit(0);
-    } else {
+    }
+    else
+    {
         // Parent only
-        printf("(pid: %o) I am parent process. The new child is pid %o\n", getpid(), pid);  
+        sprintf(buf, "(pid: %o) I am parent process. The new child is pid %o\n", getpid(), pid);
+        write(1, buf, strlen(buf));
+
     }
     wait(NULL);
-    printf("(pid: %o) I know that the child has completed.\n", getpid());
+    sprintf(buf, "(pid: %o) I know that the child has completed.\n", getpid());
+    write(1, buf, strlen(buf));
+
     exit(0);
-} 
+}
--- c/prod_con.c
+++ c/prod_con.c
@@ -2,44 +2,66 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
+
+char buf[100];      // For printing
+char buffer[100];  // Pipe Message
+int pipe_info[2]; // Used to store two ends of first pipe
+
+void l(char *msg) {
+    sprintf(buf, "%s (pinf: %d, %d)\n", msg, pipe_info[0], pipe_info[1]);
+    write(1, buf, strlen(buf));
+}
 
 void producer(int pipe_info[])
 {
-    printf("I am producer\n");
+    l("producer...");
     write(pipe_info[1], "hello", 5);
+    sprintf(buf, "** Producer sent: %s\n", "hello");
+    write(1, buf, strlen(buf));
 }
 
 void consumer(int pipe_info[])
 {
-    printf("I am consumer\n");
-    char buffer[100];
-    // read(pipe_info[0], buffer, 100);
-    // printf("Received: %s\n", buffer);
+    l("consumer...");
+    read(pipe_info[0], buffer, 100);
+    sprintf(buf, "** Consumer received: %s\n", buffer);
+    write(1, buf, strlen(buf));
 }
 
 int main()
 {
-    int pipe_info[2]; // Used to store two ends of first pipe
+    l("starting...");
+
+    pipe(pipe_info);  // Creates a new pipe.  File descriptors for the two ends of the pipe are placed in pipefd.
+
+    l("Pipe is built");
+
     int pid1 = fork();
     if (pid1 == 0)
     {
-        // Child 1, which will be the consumer
+        // In Child 1, which will be the consumer
         consumer(pipe_info);
         exit(0);
     }
     else
     {
+        // Still in parent process
         int pid2 = fork();
-        printf("%d %d", pid1, pid2);
+        sprintf(buf, "else: pids: %d %d\n", pid1, pid2);
+        write(1, buf, strlen(buf));
         if (pid2 == 0)
         {
-            // Child 2, which will be the producer
+            // In Child 2, which will be the producer
             producer(pipe_info);
             exit(0);
         }
     }
     // Parent
-    printf("Both children have completed.\n");
+    l("parent waiting...");
+
     wait(NULL);
+    l("parent done waiting, exiting...");
     exit(0);
 }
+
